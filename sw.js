e403862349763f1608a53c7892560188d054dfc9
Raazim tech <raazimtech@gmail.com>
2026-08-24 05:35:41 +0300

    Add Haga PWA service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'haga-v1';
+const APP_SHELL = ['./','./index.html','./style.css','./app.js','./config.js','./logo.svg','./manifest.webmanifest'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html'))));
+  }
+});
